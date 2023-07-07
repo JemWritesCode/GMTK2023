2fab576230cd4a17c36fa523e9116af8df1da8a9
Jem Dawson <thejemmeh@gmail.com>
2023-07-07 16:57:59 -0700

    Basic character control. Still needs some refining tho

---
+++ GMTK2023/Assets/Scripts/Pickup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // When hovered over, show the [E] to interact button
+
+
+    // isPickup?
+    // some kind of effect on pickups. Maybe just a particle effect
+    // if they pick it up then put the prefab in the bird's mouth (this will have to be done to make sure the player can tell they picked it up)
+
+    // when picked up, change on the Player what object they are holding
+    // and delete the object from it's place in the world
+}
