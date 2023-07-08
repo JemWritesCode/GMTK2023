fc7f1e8eda7d3ca74fb304d5f1b406846d8d620d
redseiko <redseiko@gmail.com>
2023-07-07 23:27:10 -0700

    Create example YoloQuest and DialogNode.

--- GMTK2023/Assets/Scripts/Game/Dialog/DialogNode.cs
+++ GMTK2023/Assets/Scripts/Game/Dialog/DialogNode.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 
-public class DialogNode : ScriptableObject { 
+[CreateAssetMenu(menuName = "Dialog/DialogNode")]
+public class DialogNode : ScriptableObject {
+  [field: SerializeField, TextArea()]
+  public string QuestStartText { get; private set; }
 
+  [field: SerializeField]
+  public QuestItemData QuestItemNeeded { get; private set; }
 }
