995634dcb856d809b1e84443c1e8093a84ed3b54
redseiko <redseiko@gmail.com>
2023-07-07 17:42:16 -0700

    Check in empty QuestItemPanelController.

--- GMTK2023/Assets/Scripts/UI/DialogPanelController.cs
+++ GMTK2023/Assets/Scripts/UI/DialogPanelController.cs
@@ -1,6 +1,18 @@
+using TMPro;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogPanelController : MonoBehaviour {
   [field: SerializeField]
-  public CanvasGroup PanelCanvasGroup { get; private set; }
+  public CanvasGroup DialogPanel { get; private set; }
+
+  [field: SerializeField]
+  public TMP_Text DialogText { get; private set; }
+
+  [field: SerializeField]
+  public Button ConfirmButton { get; private set; }
+
+  [field: SerializeField]
+  public TMP_Text ConfirmButtonLabel { get; private set; }
 }
