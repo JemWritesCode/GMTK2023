5959786a0b7f80d565379013cf8d3dd3d838b815
redseiko <redseiko@gmail.com>
2023-07-08 14:43:01 -0700

    Add DialogNodeType to DialogNode.
    Update quests to be FindItemQuest type.

--- GMTK2023/Assets/Scripts/Game/Dialog/DialogNode.cs
+++ GMTK2023/Assets/Scripts/Game/Dialog/DialogNode.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 
 [CreateAssetMenu(menuName = "Dialog/DialogNode")]
 public class DialogNode : ScriptableObject {
-  [field: SerializeField, TextArea()]
-  public string QuestStartText { get; private set; }
+  public enum DialogNodeType {
+    Conversation,
+    FindItemQuest
+  }
+
+  [field: SerializeField, Header("Node")]
+  public DialogNodeType NodeType { get; private set; }
 
   [field: SerializeField]
+  public DialogNode NextNode { get; private set; }
+
+  [field: SerializeField, Header("Conversation")]
+  public List<string> ConversationTexts { get; private set; }
+
+  [field: SerializeField, Header("Quest")]
   public QuestItemData QuestItemNeeded { get; private set; }
+
+  [field: SerializeField, TextArea()]
+  public string QuestStartText { get; private set; }
 
   [field: SerializeField, TextArea()]
   public string QuestPlayerMissingItemText { get; private set; }
