682b5d566b21e53d64fcf25a4ff0f28ff11219ea
Jem Dawson <thejemmeh@gmail.com>
2023-07-07 19:29:56 -0700

    scarecrow planning

---
+++ GMTK2023/Assets/Scripts/Scarecrow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scarecrow : MonoBehaviour
+{
+    // automatic opening dialog
+
+    // E to interact to Talk to the Scarecrow
+
+    // Logic for which quest you're currently on / what item you're currently looking for
+
+
+}
