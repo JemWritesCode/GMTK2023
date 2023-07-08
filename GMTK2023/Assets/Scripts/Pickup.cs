48ae129f60fadfef0b16b34cba0f31fda8828d08
Jem Dawson <thejemmeh@gmail.com>
2023-07-07 18:55:49 -0700

    when item is picked up it gets attached to the player

--- GMTK2023/Assets/Scripts/Pickup.cs
+++ GMTK2023/Assets/Scripts/Pickup.cs
@@ -13,4 +13,31 @@ public class Pickup : MonoBehaviour
 
     // when picked up, change on the Player what object they are holding
     // and delete the object from it's place in the world
+
+    [SerializeField] GameObject itemToPickup;
+    [SerializeField] AudioSource pickupSound;
+
+    private void Update()
+    {
+        grabTheItem(itemToPickup); // jemdebug
+    }
+
+    public void grabTheItem(GameObject itemToGrab)
+    {
+        // add to player inventory. I think it'll be byref based on what was being raycasted 
+        // lets not instantiate or destroy because that takes resources. simply move it's position to being attached to the bird
+
+        //GameObject.Destroy(this.gameObject);
+        itemToGrab.transform.SetParent(GameObject.FindWithTag("Player").transform); // this sets it to be a child of the player BUT it's transform is relative to the center of the player, it's not connected to the animation rig
+        PlayPickupSound();
+
+    }
+
+    private void PlayPickupSound()
+    {
+        if (pickupSound != null)
+        {
+            pickupSound.Play();
+        }
+    }
 }
