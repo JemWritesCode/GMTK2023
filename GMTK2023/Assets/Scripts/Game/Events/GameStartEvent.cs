c33ad0ffc8fd3411c7de1ca178e572eb121e938a
redseiko <redseiko@gmail.com>
2023-07-08 21:50:29 -0700

    Create GameStartEvent prefab and script and add it to the Mainlevel.

---
+++ GMTK2023/Assets/Scripts/Game/Events/GameStartEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class GameStartEvent : MonoBehaviour{
+  [field: SerializeField]
+  public DialogActor OpeningConvoActor { get; private set; }
+
+  [field: SerializeField]
+  public DialogNode OpeningConvoNode { get; private set; }
+
+  void Start() {
+    StartCoroutine(StartOpeningConvo());
+  }
+
+  IEnumerator StartOpeningConvo() {
+    yield return new WaitForSeconds(1f);
+
+    DialogManager.Instance.ProcessNode(OpeningConvoActor, OpeningConvoNode);
+  }
+}
