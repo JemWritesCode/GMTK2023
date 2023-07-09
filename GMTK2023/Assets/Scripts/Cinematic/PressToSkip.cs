c07a11e40e2d3ead48ef4504e21515cc74aac449
redseiko <redseiko@gmail.com>
2023-07-09 00:26:12 -0700

    Added PressToSkip to CinematicScene.

---
+++ GMTK2023/Assets/Scripts/Cinematic/PressToSkip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressToSkip : MonoBehaviour {
+  void LateUpdate() {
+    if (Input.GetKeyDown(KeyCode.Q)) {
+      if (_loadNextSceneCoroutine == null) {
+        _loadNextSceneCoroutine = StartCoroutine(LoadNextScene());
+      }
+    }
+  }
+
+  Coroutine _loadNextSceneCoroutine;
+
+  IEnumerator LoadNextScene() {
+    yield return null;
+    SceneManager.LoadScene("2-MainLevel");
+  }
+}
