3ccc4547090bad1882cdd275e44243f990cd3e3f
redseiko <redseiko@gmail.com>
2023-07-08 17:15:49 -0700

    Add AnimateButtonHover to StartButton.

---
+++ GMTK2023/Assets/Scripts/UI/AnimateButtonHover.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimateStart : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+  [field: SerializeField]
+  public Vector3 DoScaleEndValue { get; private set; } = Vector3.one;
+
+  [field: SerializeField, Min(0f)]
+  public float DoScaleDuration { get; private set; } = 0.5f;
+
+  private Tweener _onHoverTweener;
+
+  private void Start() {
+    _onHoverTweener =
+        transform
+            .DOScale(DoScaleEndValue, DoScaleDuration)
+            .SetLink(gameObject)
+            .SetAutoKill(false)
+            .SetUpdate(true)
+            .Pause();
+  }
+
+  public void OnPointerEnter(PointerEventData eventData) {
+    _onHoverTweener.Restart();
+  }
+
+  public void OnPointerExit(PointerEventData eventData) {
+    _onHoverTweener.SmoothRewind();
+  }
+}
