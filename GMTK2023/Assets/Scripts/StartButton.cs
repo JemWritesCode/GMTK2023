c54b3eab960f3909e523fe4900eab6442f6a1df8
Jem Dawson <thejemmeh@gmail.com>
2023-07-16 23:59:42 -0700

    Plays birdsong when you hover the start game button

--- GMTK2023/Assets/Scripts/StartButton.cs
+++ GMTK2023/Assets/Scripts/StartButton.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    [SerializeField] AudioSource startButtonSound;
+
+    public void PlayStartButtonSound()
+    {
+        startButtonSound.Play();
+    }
+
+    public void PauseStartButtonSound()
+    {
+        startButtonSound.Pause();
+    }
+
     public void StartGame()
     {
         Debug.Log("Attempting to load next scene by name: 1-CinematicOpening");
